9f50a661d09df1748c81d354d39802411f1571d1
Mike Leonides <mike@leonides.dev>
2025-02-06 16:29:49 -0600

    Edit test 2

--- pages/index.vue
+++ pages/index.vue
@@ -9,7 +9,7 @@
 						width="200"
 						src="~/assets/images/avi.jpeg"
 						alt="Head shot of Mike Leonides smiling in front of greenery. He is quite gorgeous." />
-					<h1>Howdy! My name is Mike.</h1>
+					<h1>Howdy! My name is Mike, and I build stuff for the web.</h1>
 					<p>
 						I am passionate about creating a fully accessible and resilient web
 						for all, and I support an ethical and respectful implementation of
